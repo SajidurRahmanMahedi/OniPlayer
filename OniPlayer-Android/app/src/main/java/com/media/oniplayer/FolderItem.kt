e7b6879c47af3419d8c735b162aa4322e66ba8f7
Md Mahedi <mdmahedi9348@gmail.com>
2026-08-10 17:21:26 +0600

    update

--- OniPlayer-Android/app/src/main/java/com/media/oniplayer/FolderItem.kt
+++
@@ -1,7 +0,0 @@
-package com.media.oniplayer
-
-data class FolderItem(
-    val name: String,
-    val path: String,
-    val videos: List<VideoItem>
-)
--- OniPlayer-Android/app/src/main/java/com/media/oniplayer/VideoItem.kt
+++
@@ -1,9 +0,0 @@
-package com.media.oniplayer
-
-data class VideoItem(
-    val id: Long,
-    val title: String,
-    val path: String,
-    val duration: Long,
-    val size: Long
-)
