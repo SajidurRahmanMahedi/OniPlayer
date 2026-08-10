7b3ed9aa2fba493b3aa016e64e71ac5924c43841
Md Mahedi <mdmahedi9348@gmail.com>
2026-08-10 13:15:59 +0600

    Add ffmpeg to installation dependencies in libvlc.sh

--- OniPlayer-Linux/libvlc.sh
+++ OniPlayer-Linux/libvlc.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --needed base-devel git libtool automake autoconf pkgconf gettext flex bison lua
+sudo pacman -S --needed base-devel git libtool automake autoconf pkgconf gettext flex bison lua ffmpeg
 
 git clone --depth 1 --branch 3.0.23 https://code.videolan.org/videolan/vlc.git
 cd vlc
